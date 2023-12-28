999e754a38d534870a3395ec1ab74a562019fe1d
poornimadevi22 <145578565+poornimadevi22@users.noreply.github.com>
2023-12-28 14:03:03 +0530

    add code for BMS

---
+++ models/book.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+
+
+const bookSchema= new mongoose.Schema({
+    title:String,
+    author:String,
+    description:String,
+});
+
+const Book= mongoose.model("Book",bookSchema);
+
+
+module.exports=Book;
